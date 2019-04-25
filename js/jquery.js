f24439a984df5fcc5cf2c19fd4c57874ccad0564
jck-n <jackhnelson0@gmail.com>
2019-04-25 17:54:31 -0400

    optimized mixins and media queries

--- js/jquery.js
+++ js/jquery.js
@@ -6,25 +6,23 @@ $(document).ready(function () {
         element.css("opacity", 0);
         element.addClass("scrollFadeIn");
         element.removeClass("scrollFadeOut");
-        element.delay(100);
     }
 
     function fadeElementOut(element) {
         element.css("opacity", 1);
         element.addClass("scrollFadeOut");
         element.removeClass("scrollFadeIn");
-        element.delay(100);
     }
 
     $(window).scroll(function () {
         var windowpos = $(window).scrollTop();
-        if (windowpos >= 25 && windowpos <= 1500) {
+        if (windowpos >= 0.1 && windowpos <= 1200) {
             fadeElementIn(aboutFade);
         } else {
             fadeElementOut(aboutFade);
         }
 
-        if (windowpos >= 1200) {
+        if (windowpos >= 1400) {
             fadeElementIn(convFade);
         } else {
             fadeElementOut(convFade);
